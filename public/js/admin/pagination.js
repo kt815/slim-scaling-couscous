7550d36afc155af854624ea1bdfac5474103cbad
kt815 <kt.815@mail.ru>
2016-03-18 18:34:14 +0800

    Leading commit

--- public/js/admin/pagination.js
+++ public/js/admin/pagination.js
@@ -21,11 +21,9 @@ function PaginatorObj() {
                 else {
                     console.log("hhhh ..... false");
                 }
-
             }
         });
         });
-
     }
 
     function _addContent(data) {
@@ -39,41 +37,22 @@ function PaginatorObj() {
             $( ".paginator" ).addClass( "disabled" );
         }
         
-
         myArray = myArray['jokes'];
-
+        
         myArray.forEach(function(item, myArray) {
-
+            ""
             var myVar = "<div class=\"callout\"><span class=\"fa-stack fa-lg\"><i class=\"fa fa-square-o fa-stack-2x text-grey\"><\/i><i class=\"fa fa-user-md fa-stack-1x text-blue    \"><\/i><\/span>" + item['author'] + "&nbsp;&nbsp;&nbsp;&nbsp;" + item['id'] + "<\/p><p><\/p><p class=\"lead\">"  + item['title'] +  "<\/p><p class=\"subheader\">" + item['text'] + "<\/p><\/div>";
 
-            var a = [1,4,7];
-            var b = [2,5,8];
-            var c = [3,6,9];
-
-            if ( item['index'] == 1 || item['index'] == 4 || item['index'] == 7) {
-                _addContent_OneColumn(myVar);
-            }
-            else if ( item['index'] == 2 || item['index'] == 5 || item['index'] == 8) {
-                _addContent_TwoColumn(myVar);
-            }
-            else if ( item['index'] == 3 || item['index'] == 6 || item['index'] == 9) {
-                _addContent_ThreeColumn(myVar);
-            }           
+                _addContent_Column(myVar);            
 
         });
 
+
+
     }
 
-    function _addContent_OneColumn(data) { 
-            $(".one-column").append(data);
-    }
-
-    function _addContent_TwoColumn(data) { 
-            $(".two-column").append(data);
-    }
-
-    function _addContent_ThreeColumn(data) { 
-            $(".three-column").append(data);
+    function _addContent_Column(data) { 
+            $(".jokes-column").append(data);
     }
 
     this.init = function() {
@@ -87,5 +66,3 @@ function PaginatorObj() {
 
 var PaginatorObj = new PaginatorObj();
 PaginatorObj.init();
-
-
